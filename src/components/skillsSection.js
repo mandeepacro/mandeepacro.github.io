b2be52b5cf2197d431626a4f519e3a5886e23209
mandeepacro <mandeepbaghelcs17@acropolis.in>
2024-01-21 13:06:05 +0530

    added fn code for netlify

--- src/components/skillsSection.js
+++ src/components/skillsSection.js
@@ -5,13 +5,12 @@ import * as styles from "./skillsSection.module.css";
 import { InView } from 'react-intersection-observer';
 import { Code } from 'react-content-loader'
 import Typed from "typed.js";
-import { error } from 'jquery';
+import { createSkillsPoem } from '../functions/apiCaller';
 
 const SkillsSection = () => {
   const skillsList = useRef([]);
   const el = useRef(null);
   const typed = useRef(null)
-  const apiSecretKey = process.env.GATSBYAPP_API_SECRET_KEY;
   const [poemText, setPoemText] = useState(null);
   const [showHonourableMentionText, setShowHonourableMentionText] = useState(false);
   const [skillsIgnored, setSkillsIgnored] = useState([]);
@@ -42,7 +41,6 @@ const SkillsSection = () => {
     `);
 
   useEffect(() => {
-    console.log("API key from settings: ", process.env.GATSBYAPP_API_SECRET_KEY);
     let skillsArray = data.allMarkdownRemark.nodes[0].frontmatter.list;
     let skills = [];
     skillsArray.forEach(el => {
@@ -57,7 +55,7 @@ const SkillsSection = () => {
         try {
           setPoemText("initiated");
           setDisableGenerateButton(true);
-          let result = await createCompletion();
+          let result = await createSkillsPoem(skillsList.current);
           generateNonUsedSkills(result);
           setResponse(result);
         } catch (error) {
@@ -70,90 +68,13 @@ const SkillsSection = () => {
     //get the poem response from chatGPT
   }, []); // empty dependency array to run the effect only once
 
-
-  async function createCompletion(isMock = true) {
-    prompt = `Generate a sweet, simple and fun poem of developing a fullstack website using my skills. Here are the list of skills to use - ${skillsList.current.map(obj => obj.name).join(", ")}. Note: Generate a intersesting title for the poem as the first line and then start with the poem body, Use exact words as provided in the skills to use list.`;
-
-    console.log("CALLING OPENAI API");
-    const DEFAULT_PARAMS = {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
-      temperature: 0.5
-    };
-
-    if (!isMock) {
-      const result = await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiSecretKey}`
-        },
-        body: JSON.stringify(DEFAULT_PARAMS)
-      });
-
-      const stream = result.body
-      const output = await fetchStream(stream);
-      console.log("API RESPONSE: ", output);
-      return output.choices[0].message.content;
-    }
-    else {
-      let mockPoem = `The Sweet Symphony of Fullstack Development
-  
-      With HTML and CSS, I started my quest,
-      To create a website that's simply the best.
-      JavaScript and SCSS, my trusty tools,
-      To bring my vision to life, no longer just a fool.`;
-
-      return new Promise((resolve, reject) => {
-        // Simulating asynchronous behavior
-        setTimeout(() => {
-          resolve(mockPoem); // Resolve with mock data
-        }, 1000);
-      });
-    }
-  }
-
-  async function fetchStream(stream) {
-    const reader = stream.getReader();
-    let charsReceived = 0;
-    const li = document.createElement("li");
-
-    // read() returns a promise that resolves
-    // when a value has been received
-    const result = await reader.read().then(
-      function processText({ done, value }) {
-        // Result objects contain two properties:
-        // done  - true if the stream has already given you all its data.
-        // value - some data. Always undefined when done is true.
-        if (done) {
-          //console.log("Stream complete");
-          return li.innerText;
-        }
-        // value for fetch streams is a Uint8Array
-        charsReceived += value.length;
-        const chunk = value;
-        //console.log(`Received ${charsReceived} characters so far. Current chunk = ${chunk}`);
-        li.appendChild(document.createTextNode(chunk));
-        return reader.read().then(processText);
-      });
-    const list = result.split(",")
-    const numList = list.map((item) => {
-      return parseInt(item)
-    })
-    const text = String.fromCharCode(...numList);
-    const res = JSON.parse(text);
-    return res;
-  }
-
   useEffect(() => {
-    //console.log("useEffect response - ",response);
     if (response && sectionIsVisible) {
       handleSectionVisibility(true);
     }
   }, [response]);
 
   useEffect(() => {
-    console.log("useEffect poemText - ", poemText);
     if (poemText && poemText != "initiated") {
       //if(typed.current == null){
       typed.current = null;
@@ -228,7 +149,7 @@ const SkillsSection = () => {
     setShowHonourableMentionText(false);
 
     try {
-      let result = await createCompletion(false);
+      let result = await createSkillsPoem(skillsList.current,false);
       generateNonUsedSkills(result);
       setResponse(result);
     } catch (error) {
