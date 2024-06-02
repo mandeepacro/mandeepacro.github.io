25ebbd26dfa20768ab71fe3931253963928d3d03
mandeepacro <mandeepbaghelcs17@acropolis.in>
2024-06-03 01:26:35 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,5 +61,21 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GA_ID, // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ]
 };
